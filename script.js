e86df1e391d5854e1c74f6805f3b8ad90fd33b1e
nukte-y <nukteu@gmail.com>
2024-01-26 16:01:50 +0000

    added parameters to APIs to get forecast data

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ $("#search-button").on("click",function (event) {
   console.log(input);
   var locQueryURL="https://eu1.locationiq.com/v1/search?key=pk.6518198b4777ed389d4ba7bb066ef984&q=" + input + "&format=json&limit=1" 
   // added limit parameter to limit the results as 1  
-  
-  fetch(locQueryURL)
-.then(function(response) {
-  return response.json();
-}).then(function(data) {
-  console.log(data);
-});
-
-})
-
-
-
-// var locQueryURL="https://eu1.locationiq.com/v1/search?key=pk.6518198b4777ed389d4ba7bb066ef984&q=london&format=json"
+    fetch(locQueryURL)
+    .then(function(response) {
+      return response.json();})
+      .then(function(data) {
+        var lat=data[0].lat;      //getting geographic coordinates
+        var lon=data[0].lon;      //to use as a paremeter in my weather forecast URL
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d7eab80b6a689fcdafaedb821b3c11eb";
+        fetch(queryURL)
+          .then(function(weatherObject){
+            var object = weatherObject.json();
+            console.log(object);
+          }
+        )
+      }
+    )
+  }
+);
 
