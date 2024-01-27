94fa31d16a3359e6ab8c2e6f08d965efe051f535
nukte-y <nukteu@gmail.com>
2024-01-27 22:56:38 +0000

    added render function and rules for history buttons

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 var storageArray=[];
 if(localStorage.length!==0){
   storageArray=JSON.parse(localStorage.getItem("storageArray"));  //getting stored array
-    for(i=0;i<storageArray.length;i++){
-      $("<button>").text(storageArray[i]).appendTo("#history");
-    }
-
+  render(storageArray);
 }
 
+function render(array){
+  $("#history").empty();
+  for(i=array.length-1;i>=0;i--){         
+    if(i<7){                    //limited history display
+     $("<button>").text(array[i]).addClass("historyButton").appendTo("#history");
+    }
+ }
+}
 
-$("#search-button").on("click",function (event) {
-  event.preventDefault();
-  var input=$("#search-input").val().trim();   //getting input from user and use it in URL to search and get the related data
-  storageArray.push(input);                    //add input to the array
-  var string=JSON.stringify(storageArray);      //convert array to the string
-  localStorage.setItem("storageArray",string);  //add array into localStorage
-  var buttonEl=$("<button>").appendTo("#history").text(input);
+function search(input){
   var locQueryURL="https://eu1.locationiq.com/v1/search?key=pk.6518198b4777ed389d4ba7bb066ef984&q="+ input + 
   "&format=json&limit=1" // added limit parameter to limit the results as 1  
     fetch(locQueryURL)
@@ -25,13 +24,37 @@ $("#search-button").on("click",function (event) {
         var lon=data[0].lon;      //to use as a paremeter in my weather forecast URL
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=d7eab80b6a689fcdafaedb821b3c11eb";
         fetch(queryURL)
-          .then(function(weatherObject){
-            var object = weatherObject.json();
-            console.log(object);
-          }
-        )
-      }
-    )
+          .then(function(weatherResponse){
+            return weatherResponse.json();
+          })
+        .then(function(data){
+          console.log(data);
+          console.log(data.list[0].dt_txt);
+          console.log(new Date(data.list[0].dt))
+          let today = new Date(data.list[0].dt_txt)
+          let dt = today.getUTCDate();
+          console.log(new Date(data.list[0].dt_txt) )
+          console.log(today.getFullYear() +" " +today.getMonth()+1 +" " + today.getDay() );
+          console.log(data.list[0].dt_txt.substring(0,10))
+        })
+      })
+}
+
+$("#search-button").on("click",function (event) {
+  event.preventDefault();
+  let input=$("#search-input").val().trim();   //getting input from user and use it in URL to search and get the related data
+    if(!storageArray.includes(input)){             //check if the city is already in history
+      storageArray.push(input);                    //add input to the array
+      render(storageArray);
+    }
+  var string=JSON.stringify(storageArray);      //convert array to the string
+  localStorage.setItem("storageArray",string);  //add array into localStorage
+  search(input);
   }
 );
 
+$("#history").on("click",".historyButton",function(event){
+  event.preventDefault();
+  let input=$(this).text();
+  search(input);
+})
