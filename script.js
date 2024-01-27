a9b167d3961b896d7c8676fdadc68a813140b871
nukte-y <nukteu@gmail.com>
2024-01-27 12:01:19 +0000

    added inputs to the localStorage and used JSON to get them

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=d7eab80b6a689fcdafaedb821b3c11eb";
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=51.5156177&lon=-0.0919983&appid=d7eab80b6a689fcdafaedb821b3c11eb";
+var storageArray=[];
+if(localStorage.length!==0){
+  storageArray=JSON.parse(localStorage.getItem("storageArray"));  //getting stored array
+  console.log(storageArray);
+}
 
 
 $("#search-button").on("click",function (event) {
   event.preventDefault();
-  var input=$("#search-input").val();   //getting input from user and use it in URL to search and get the related data
-  console.log(input);
-  var locQueryURL="https://eu1.locationiq.com/v1/search?key=pk.6518198b4777ed389d4ba7bb066ef984&q=" + input + "&format=json&limit=1" 
-  // added limit parameter to limit the results as 1  
+  var input=$("#search-input").val().trim();   //getting input from user and use it in URL to search and get the related data
+  storageArray.push(input);                    //add input to the array
+  var string=JSON.stringify(storageArray);      //convert array to the string
+  localStorage.setItem("storageArray",string);  //add array into localStorage
+  var buttonEl=$("<button>").appendTo("#history").text(input);
+  var locQueryURL="https://eu1.locationiq.com/v1/search?key=pk.6518198b4777ed389d4ba7bb066ef984&q="+ input + 
+  "&format=json&limit=1" // added limit parameter to limit the results as 1  
     fetch(locQueryURL)
     .then(function(response) {
       return response.json();})
